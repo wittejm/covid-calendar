2c7b905d0e62f455db3af0cde37962b108d4dd69
Jordan Witte <wittejm@gmail.com>
2020-08-28 18:44:54 -0700

    Some starting visuals
    Some initial interactivity on the "Household".

---
+++ src/types.ts
@@ -0,0 +1,30 @@
+export interface HouseholdData {
+  members : PersonData[]
+}
+
+export interface PersonData {
+  name: string;
+  covidEvents : CovidEvent[]
+}
+
+export interface CovidEvent {
+  name: string
+}
+
+export const LastExposure = {
+  name: "Last Exposure"
+}
+export const NewSymptoms = {
+  name: "New Symptoms"
+}
+export const SymptomsResolve = {
+  name: "Symptoms Resolve"
+}
+export const PositiveTest = {
+  name: "Positive Test"
+}
+export const NegativeTest = {
+  name: "Negative Test"
+}
+
+export const AllEvents = [LastExposure, NewSymptoms, SymptomsResolve, PositiveTest, NegativeTest]
