f071f013c21697531facea6790b8c4fd667d47a8
wittejm <jordan.witte@codeforpdx.org>
2021-01-28 22:22:48 +0000

    Deploying to gh-pages from  @ 002acb9d00abcf20b07973081a1fe9177e38a185 🚀

---
+++ precache-manifest.3b93127e2de2ad6b2fa69d111970b006.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b17aef025330281d4540ad16969dea05",
+    "url": "/covid-calendar/index.html"
+  },
+  {
+    "revision": "00f49fa32b6d7f1ff03f",
+    "url": "/covid-calendar/static/css/2.a9b1b95d.chunk.css"
+  },
+  {
+    "revision": "7ac4bbeee3a9c9e51cff",
+    "url": "/covid-calendar/static/css/main.6ddc3623.chunk.css"
+  },
+  {
+    "revision": "00f49fa32b6d7f1ff03f",
+    "url": "/covid-calendar/static/js/2.414e3364.chunk.js"
+  },
+  {
+    "revision": "80c960e1dc69692b426ead9608a8707a",
+    "url": "/covid-calendar/static/js/2.414e3364.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ac4bbeee3a9c9e51cff",
+    "url": "/covid-calendar/static/js/main.b3a40949.chunk.js"
+  },
+  {
+    "revision": "fd13238340c7c884c719",
+    "url": "/covid-calendar/static/js/runtime-main.d772ebd2.js"
+  },
+  {
+    "revision": "085b1dd8427dbeff10bd55410915a3f6",
+    "url": "/covid-calendar/static/media/fa-brands-400.085b1dd8.ttf"
+  },
+  {
+    "revision": "0fabb6606be4c45acfeedd115d0caca4",
+    "url": "/covid-calendar/static/media/fa-brands-400.0fabb660.eot"
+  },
+  {
+    "revision": "cac68c831145804808381a7032fdc7c2",
+    "url": "/covid-calendar/static/media/fa-brands-400.cac68c83.woff2"
+  },
+  {
+    "revision": "ccfdb9dc442be0c629d331e94497428b",
+    "url": "/covid-calendar/static/media/fa-brands-400.ccfdb9dc.svg"
+  },
+  {
+    "revision": "dc0bd022735ed218df547742a1b2f172",
+    "url": "/covid-calendar/static/media/fa-brands-400.dc0bd022.woff"
+  },
+  {
+    "revision": "05b53beb21e3ef13d28244545977152d",
+    "url": "/covid-calendar/static/media/fa-regular-400.05b53beb.woff"
+  },
+  {
+    "revision": "1a78af4105d4d56e6c34f76dc70bf1bc",
+    "url": "/covid-calendar/static/media/fa-regular-400.1a78af41.ttf"
+  },
+  {
+    "revision": "3a3398a6ef60fc64eacf45665958342e",
+    "url": "/covid-calendar/static/media/fa-regular-400.3a3398a6.woff2"
+  },
+  {
+    "revision": "ad3a7c0d77e09602f4ab73db3660ffd8",
+    "url": "/covid-calendar/static/media/fa-regular-400.ad3a7c0d.eot"
+  },
+  {
+    "revision": "e75dfd904d366a2560c63c23cfc98ef8",
+    "url": "/covid-calendar/static/media/fa-regular-400.e75dfd90.svg"
+  },
+  {
+    "revision": "03ba7cb710104df27f1c9c46d64bee4e",
+    "url": "/covid-calendar/static/media/fa-solid-900.03ba7cb7.svg"
+  },
+  {
+    "revision": "781e85bb50c8e8301c30de56b31b1f04",
+    "url": "/covid-calendar/static/media/fa-solid-900.781e85bb.ttf"
+  },
+  {
+    "revision": "89bd2e38475e441a5cd70f663f921d61",
+    "url": "/covid-calendar/static/media/fa-solid-900.89bd2e38.eot"
+  },
+  {
+    "revision": "c500da19d776384ba69573ae6fe274e7",
+    "url": "/covid-calendar/static/media/fa-solid-900.c500da19.woff2"
+  },
+  {
+    "revision": "ee09ad7553b8ad3d81150d609d5341a0",
+    "url": "/covid-calendar/static/media/fa-solid-900.ee09ad75.woff"
+  }
+]);
